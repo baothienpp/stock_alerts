ff113d8dd6cf4260d38603b17bd4e2753d89f435
Bao-Thien Nguyen <nguyen@future-demand.com>
2021-02-16 13:06:23 +0100

    change docker

--- scripts/run_kafka.sh
+++ scripts/run_kafka.sh
@@ -1,3 +1,8 @@
 mkdir -p ~/kafka-data
+mkdir -p ~/kafka-data/logs
+mkdir -p ~/kafka-data/settings
+
+docker run -d -p 2181:2181 -p 9092:9092 --env ADVERTISED_HOST=172.17.0.1 --env ADVERTISED_PORT=9092 spotify/kafka
 #docker run -d -v ~/kafka-data:/tmp/kafka-logs -p 2181:2181 -p 9092:9092 --env ADVERTISED_HOST=172.17.0.1 --env ADVERTISED_PORT=9092 spotify/kafka
-docker run -d -v ~/kafka-data:/tmp/kafka-logs -p 2181:2181 -p 9092:9092 --env ADVERTISED_HOST=127.0.0.1 --env ADVERTISED_PORT=9092 spotify/kafka
+
+
